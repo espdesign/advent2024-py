bf3ef3bb6e53a64f897250156d96edcc10d73978
espdesign <evanpendergraft@gmail.com>
2024-12-01 09:04:47 -0500

    add day 1.

---
+++ day1/day1.py
@@ -0,0 +1,26 @@
+day_data: list = open("input", "r").read().split()
+
+ll = []
+rl = []
+
+index = 0
+for i in day_data:
+    if index % 2 == 0:
+        ll.append(i)
+    else:
+        rl.append(i)
+    index += 1
+
+ll.sort()
+rl.sort()
+
+result = []
+
+for i in range(0, len(ll)):
+    x = int(ll[i])
+    y = int(rl[i])
+    if x > y:
+        result.append(x - y)
+    else:
+        result.append(y - x)
+print(sum(result))
