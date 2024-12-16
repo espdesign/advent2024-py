560106915fd2de96d25a1e33534b2c2ebf454bfd
espdesign <evanpendergraft@gmail.com>
2024-12-16 11:53:09 -0500

    day 8 complete.

--- day8/day8-part2.py
+++ day8/day8-part2.py
@@ -4,25 +4,7 @@ from itertools import combinations
 debug = False
 if debug:
     file_path = "example.input"
-    ## ADD ANTINODE DATA
-    file_path2 = "example-a.input"
-    file2 = open(file_path2, "r")
-    anti_node_data = file2.read().split("\n")
-    file2.close()
 
-    anti_node_data_puzzle_map = {}
-    a = 0
-    for row in anti_node_data:
-        b = 0
-        for character in row:
-            anti_node_data_puzzle_map[(a, b)] = anti_node_data[a][b]
-            b += 1
-        a += 1
-
-    for i in anti_node_data_puzzle_map:
-        if anti_node_data_puzzle_map[i] == "#":
-            print(i)
-    pp(anti_node_data)
 
 else:
     file_path = "input.input"
@@ -47,20 +29,37 @@ for i in puzzle_map:
         freqs[i] = puzzle_map[i]
 
 
-def calc_anti_nodes(p1, p2):
+def calc_anti_nodes(p1, p2, con):
+    nodes = []
+    inside_constraints = True
     diff = (p2[0] - p1[0], p2[1] - p1[1])
     an1 = p1[0] - diff[0], p1[1] - diff[1]
     an2 = p2[0] + diff[0], p2[1] + diff[1]
-    return [an1, an2]
+    while inside_constraints:
+        an1_valid = False
+        an2_valid = False
+        if valid_anti_nodes(con, an1):
+            nodes.append(an1)
+            an1_valid = True
+            p1 = an1
+            an1 = p1[0] - diff[0], p1[1] - diff[1]
+        if valid_anti_nodes(con, an2):
+            nodes.append(an2)
+            an2_valid = True
+            p2 = an2
+            an2 = p2[0] + diff[0], p2[1] + diff[1]
+        if not an1_valid and not an2_valid:
+            inside_constraints = False
+    return nodes
 
 
 def valid_anti_nodes(con, point):
-    print(f"Testing {point} is valid. with constraint {con}")
     if point[0] < 0 or point[1] < 0:
         return False
     if point[0] >= a or point[1] >= a:
         return False
     else:
+        print(f"{point} is valid in constraint of {con}")
         return True
 
 
@@ -74,23 +73,18 @@ pairs_by_group = {
 
 
 result = []
+# Output the results
 for value, pairs in pairs_by_group.items():
     print(f"Pairs for '{value}': {pairs}")
+    # we need to add the nodes themselves if they are valid combinations.
     for pair in pairs:
-        anx = calc_anti_nodes(pair[0], pair[1])
-        if valid_anti_nodes(a, anx[0]):
-            result.append(anx[0])
-        if valid_anti_nodes(a, anx[1]):
-            result.append(anx[1])
+        for i in pair:
+            result.append(i)
+        anx = calc_anti_nodes(pair[0], pair[1], a)
+        for i in anx:
+            result.append(i)
 
-
+# remove duplicates
 result_unique = list(set(result))
 
 print(len(result_unique))
-### WORKING ANTINODE CALC
-# p1 = (3, 7)
-# p2 = (2, 5)
-
-# diff = (p2[0] - p1[0], p2[1] - p1[1])
-# print(p1[0] - diff[0], p1[1] - diff[1])
-# print(p2[0] + diff[0], p2[1] + diff[1])
